5d89f7559133bddf24761a7b1c402b0a9ba52a49
moses <mosestirumani@gmail.com>
2020-06-13 19:36:23 +0530

    gasbill

---
+++ stream-impl/src/main/java/phonepe/stream/impl/StreamModule.java
@@ -0,0 +1,21 @@
+package phonepe.stream.impl;
+
+import com.google.inject.AbstractModule;
+import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
+import phonepe.hello.api.HelloService;
+import phonepe.stream.api.StreamService;
+
+/**
+ * The module that binds the StreamService so that it can be served.
+ */
+public class StreamModule extends AbstractModule implements ServiceGuiceSupport {
+  @Override
+  protected void configure() {
+    // Bind the StreamService service
+    bindService(StreamService.class, StreamServiceImpl.class);
+    // Bind the HelloService client
+    bindClient(HelloService.class);
+    // Bind the subscriber eagerly to ensure it starts up
+    bind(StreamSubscriber.class).asEagerSingleton();
+  }
+}
